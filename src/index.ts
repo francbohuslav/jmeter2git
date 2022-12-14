e88a257b9b22f2c9cef259b0a18604f76480326f
Bohuslav Franc <bohuslav.franc@unicornuniverse.eu>
2022-12-14 20:19:52 +0100

    splitting

--- src/index.ts
+++ src/index.ts
@@ -1,159 +1,50 @@
-import commandLineArgs from "command-line-args";
-import { DOMParser, XMLSerializer } from "xmldom";
-import xpath from "xpath";
+import commandLineArgs, { OptionDefinition } from "command-line-args";
 import core from "./core";
+import { Splitter } from "./splitter";
 
-const optionDefinitions = [{ name: "jmxFile", alias: "j", type: String }];
+const optionDefinitions: OptionDefinition[] = [
+  { name: "file", alias: "f", type: String },
+  { name: "split", alias: "s", type: Boolean },
+  { name: "join", alias: "j", type: Boolean },
+];
 const options = commandLineArgs(optionDefinitions);
 
-function getPath(node: Node) {
-  let path = "";
-  if (node.parentNode != null) {
-    path += getPath(node.parentNode) + " > ";
-  }
-  path += node.nodeName;
-  return path;
+// options.file = "C:\\Gateway\\v5_X\\uu_energygateway_messageregistryg01\\uu_energygateway_messageregistryg01-server\\src\\test\\jmeter\\datagateway.jmx";
+
+// function getPath(node: Node) {
+//   let path = "";
+//   if (node.parentNode != null) {
+//     path += getPath(node.parentNode) + " > ";
+//   }
+//   path += node.nodeName;
+//   return path;
+// }
+
+// function hasAncestor(element: Element, tagName: string): boolean {
+//   element = element.parentElement;
+//   if (!element) {
+//     return false;
+//   }
+//   if (element.nodeName === tagName) {
+//     return true;
+//   }
+//   return hasAncestor(element, tagName);
+// }
+
+// function print(something: any) {
+//   if (something) {
+//     if (isElement(something)) {
+//       return something.tagName;
+//     }
+//     return (something as Object).constructor.name;
+//   }
+//   return something;
+// }
+
+if (options.split && options.file) {
+  const splitter = new Splitter(options.file);
+  splitter.splitToParts();
+} else if (options.join && options.file) {
+} else {
+  core.showError("For split: node index.js -f someFile.jmx -s\nFor join: node index.js -f someFile.jmx -j");
 }
-
-function isElement(node: any): node is Element {
-  return node instanceof Object && node.nodeType === 1;
-}
-
-function hasAncestor(element: Element, tagName: string): boolean {
-  element = element.parentElement;
-  if (!element) {
-    return false;
-  }
-  if (element.nodeName === tagName) {
-    return true;
-  }
-  return hasAncestor(element, tagName);
-}
-
-function print(something: any) {
-  if (something) {
-    if (isElement(something)) {
-      return something.tagName;
-    }
-    return (something as Object).constructor.name;
-  }
-  return something;
-}
-
-function prevElement(element: any): Element | null {
-  if (!element.previousSibling) {
-    return null;
-  }
-  if (isElement(element.previousSibling)) {
-    return element.previousSibling;
-  }
-  return prevElement(element.previousSibling);
-}
-
-function nextElement(element: any): Element | null {
-  if (!element.nextSibling) {
-    return null;
-  }
-  if (isElement(element.nextSibling)) {
-    return element.nextSibling;
-  }
-  return nextElement(element.nextSibling);
-}
-
-function isHashTree(element: Element): boolean {
-  return element.tagName === "hashTree";
-}
-
-function isController(element: Element): boolean {
-  return element.tagName === "kg.apc.jmeter.control.ParameterizedController";
-}
-
-function noHashTree(elements: Element[]): Element[] {
-  return elements.filter((e) => !isHashTree(e));
-}
-
-function getChildElements(element: Element): Element[] {
-  const elements: Element[] = [];
-  for (let i = 0; i < element.childNodes.length; i++) {
-    const child = element.childNodes.item(i);
-    if (isElement(child)) {
-      elements.push(child);
-    }
-  }
-  return elements;
-}
-
-function getClosestController(element: Node): Element | null {
-  if (!element.parentNode) {
-    return null;
-  }
-  const prevEl = prevElement(element.parentNode);
-  if (prevEl && isController(prevEl)) {
-    return prevEl;
-  }
-  return getClosestController(element.parentNode);
-}
-
-(async () => {
-  const dom = new DOMParser().parseFromString(core.readTextFile(options.jmxFile));
-
-  const nodes = xpath.select("//kg.apc.jmeter.control.ParameterizedController", dom);
-  let elements: Element[] = nodes.filter((some) => {
-    if (!(some instanceof Object)) {
-      return false;
-    }
-    if (!("nodeType" in some)) {
-      return false;
-    }
-    const element = some as Element;
-    if (hasAncestor(element, "kg.apc.jmeter.control.ParameterizedController")) {
-      return false;
-    }
-    const next = nextElement(element);
-    if (isHashTree(next)) {
-      const childElements = noHashTree(getChildElements(next));
-      if (childElements.length === 1 && childElements[0].tagName === "ModuleController") {
-        return false;
-      }
-    }
-    return true;
-  }) as any;
-  var exclude = new Set<Element>();
-  for (const element of elements) {
-    const controller = getClosestController(element);
-    if (controller) {
-      exclude.add(controller);
-    }
-    // if (element.childNodes) {
-    //   for (let i = 0; i < element.childNodes.length; i++) {
-    //     const child = element.node;
-    //     child.remove();
-    //   }
-    // }
-  }
-  elements = elements.filter((e) => !exclude.has(e));
-  for (const element of elements) {
-    console.log(getPath(element) + " " + element.getAttribute("testname"));
-    element.textContent = "";
-    const next = nextElement(element);
-    if (isHashTree(next)) {
-      next.textContent = "";
-    }
-  }
-  const serializer = new XMLSerializer();
-
-  core.writeTextFile(options.jmxFile + ".desc.xml", serializer.serializeToString(dom));
-
-  // nodes.forEach((n) =>
-  //   results.push({
-  //     info: {
-  //       label: n.getAttribute("lb"),
-  //       asserts: xpath
-  //         .select("./assertionResult", n)
-  //         .filter((fn) => xpath.select1("./failure", fn).textContent == "true")
-  //         .map((fn) => xpath.select1("./failureMessage", fn).textContent.replace(/[\r\n]+/g, " ")),
-  //       responseData: xpath.select1("./responseData", n).textContent,
-  //     },
-  //     success: n.getAttribute("s") == "true",
-  //   })
-})();
---
+++ src/splitter.ts
@@ -0,0 +1,172 @@
+import fs from "fs";
+import path from "path";
+import { DOMParser, XMLSerializer } from "xmldom";
+import xpath from "xpath";
+import core from "./core";
+const md5 = require("md5");
+
+export class Splitter {
+  dom: Document;
+
+  public constructor(private filePath: string) {}
+
+  public splitToParts() {
+    const dom = new DOMParser().parseFromString(core.readTextFile(this.filePath), "application/xml");
+    const controllers = this.getControllers(dom);
+    this.checkDuplicites(controllers);
+    this.exportToParts(controllers, dom);
+  }
+
+  private getControllers(dom: Document): Element[] {
+    const nodes = xpath.select("//kg.apc.jmeter.control.ParameterizedController", dom);
+    let elements: Element[] = nodes.filter((some) => {
+      if (!(some instanceof Object)) {
+        return false;
+      }
+      if (!this.isElement(some)) {
+        return false;
+      }
+      const element = some as Element;
+      const next = this.nextElement(element);
+      if (this.isHashTree(next)) {
+        const childElements = this.noHashTree(this.getChildElements(next));
+        if (childElements.length === 1 && childElements[0].tagName === "ModuleController") {
+          // console.log("Removed " + element.getAttribute("testname"));
+          return false;
+        }
+      }
+      return true;
+    }) as any;
+
+    // Remove parent controllers
+    var exclude = new Set<Element>();
+    for (const element of elements) {
+      const controller = this.getClosestController(element);
+      if (controller) {
+        exclude.add(controller);
+      }
+    }
+    return elements.filter((e) => !exclude.has(e));
+  }
+
+  private checkDuplicites(controllers: Element[]) {
+    const controllersCountByName: { [testName: string]: number } = {};
+    for (const element of controllers) {
+      const testName = element.getAttribute("testname");
+      if (!controllersCountByName[testName]) {
+        controllersCountByName[testName] = 0;
+      }
+      controllersCountByName[testName]++;
+    }
+
+    let found = false;
+    Object.entries(controllersCountByName).forEach(([testName, count]) => {
+      if (count > 1) {
+        if (!found) {
+          core.showMessage("Duplicate names of controllers found:");
+        }
+        found = true;
+        console.log(`  ${count}x ${testName}`);
+      }
+    });
+    if (found) {
+      core.showError("ERROR: Controller name must be unique.");
+    }
+  }
+
+  private exportToParts(controllers: Element[], dom: Document) {
+    // Replace nodes
+    const dirPath = this.filePath + "-parts";
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath);
+    }
+    fs.readdirSync(dirPath)
+      .filter((f) => f.endsWith(".xml"))
+      .forEach((f) => fs.unlinkSync(path.join(dirPath, f)));
+    core.showMessage("Exporting:");
+    for (const element of controllers) {
+      const next = this.nextElement(element);
+      if (this.isHashTree(next)) {
+        const testname = element.getAttribute("testname");
+        const partFileName = md5(testname) + ".xml";
+        console.log(`  \x1b[32m${testname}\x1b[0m to \x1b[33m${partFileName}\x1b[0m`);
+        const partFilePath = path.join(dirPath, partFileName);
+        core.writeTextFile(partFilePath, `<?xml version="1.0" encoding="UTF-8"?>\n<root>\n${this.serialize(element)}\n${this.serialize(next)}\n</root>`);
+        let includeNode = dom.createElement("jmeter2git.controller");
+        includeNode.setAttribute("testname", element.getAttribute("testname"));
+        includeNode.setAttribute("filename", partFileName);
+        next.parentNode.replaceChild(includeNode, element);
+        next.parentNode.removeChild(next);
+      }
+    }
+    console.log(`\x1b[32mWorkspace\x1b[0m to \x1b[33m_workspace.xml\x1b[0m`);
+    core.writeTextFile(path.join(dirPath, "_workspace.xml"), this.serialize(dom));
+  }
+
+  private isElement(node: any): node is Element {
+    return node instanceof Object && node.nodeType === 1;
+  }
+
+  private nextElement(element: any): Element | null {
+    if (!element.nextSibling) {
+      return null;
+    }
+    if (this.isElement(element.nextSibling)) {
+      return element.nextSibling;
+    }
+    return this.nextElement(element.nextSibling);
+  }
+
+  private isHashTree(element: Element): boolean {
+    return element.tagName === "hashTree";
+  }
+
+  private isController(element: Element): boolean {
+    return element.tagName === "kg.apc.jmeter.control.ParameterizedController";
+  }
+
+  private noHashTree(elements: Element[]): Element[] {
+    return elements.filter((e) => !this.isHashTree(e));
+  }
+
+  private getChildElements(element: Element): Element[] {
+    const elements: Element[] = [];
+    for (let i = 0; i < element.childNodes.length; i++) {
+      const child = element.childNodes.item(i);
+      if (this.isElement(child)) {
+        elements.push(child);
+      }
+    }
+    return elements;
+  }
+
+  private getClosestController(element: Node): Element | null {
+    if (!element.parentNode) {
+      return null;
+    }
+    const prevEl = this.prevElement(element.parentNode);
+    if (prevEl && this.isController(prevEl)) {
+      return prevEl;
+    }
+    return this.getClosestController(element.parentNode);
+  }
+
+  private prevElement(element: any): Element | null {
+    if (!element.previousSibling) {
+      return null;
+    }
+    if (this.isElement(element.previousSibling)) {
+      return element.previousSibling;
+    }
+    return this.prevElement(element.previousSibling);
+  }
+
+  private serialize(element: Element | Document): string {
+    const serializer = new XMLSerializer();
+    return this.fixTags(serializer.serializeToString(element));
+  }
+
+  private fixTags(xml: string): string {
+    return xml.replace(/(<stringProp\s[^>]+)\/>/g, "$1></stringProp>");
+  }
+}
